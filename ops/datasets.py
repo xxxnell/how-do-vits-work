8e1e307101bdc8a8d6d34ecae2e8a22db7e1d127
XXXNELL <xxxxxnell@gmail.com>
2020-12-31 21:56:18 +0900

    Change crop_ratio → padding in datasets

--- ops/datasets.py
+++ ops/datasets.py
@@ -7,24 +7,21 @@ import torchvision.transforms as transforms
 import ops.cifarc as cifarc
 
 
-def get_dataset(name, mean, std,
-                crop_ratio=(0.875, 0.875), root="./data", download=False):
-    dataset_train, dataset_test = None, None
+def get_dataset(name,
+                root="./data", download=False, **kwargs):
     if name in ["cifar", "cifar10", "cifar-10"]:
-        dataset_train, dataset_test = get_cifar10(mean, std, crop_ratio=crop_ratio, root=root, download=download)
+        dataset_train, dataset_test = get_cifar10(root=root, download=download, **kwargs)
     elif name in ["cifar100", "cifar-100"]:
-        dataset_train, dataset_test = get_cifar100(mean, std, crop_ratio=crop_ratio, root=root, download=download)
+        dataset_train, dataset_test = get_cifar100(root=root, download=download, **kwargs)
     else:
         raise NotImplementedError
     return dataset_train, dataset_test
 
 
-def get_cifar10(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010),
-                crop_ratio=(0.875, 0.875), root="./data", download=False):
-    crop_size = (np.array((32, 32)) * (1 - np.array(crop_ratio))).astype(int)
-
+def get_cifar10(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010), padding=(4, 4),
+                root="./data", download=False):
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=tuple(crop_size)),
+        transforms.RandomCrop(32, padding=tuple(padding)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean, std),
@@ -40,12 +37,10 @@ def get_cifar10(mean=(0.4914, 0.4822, 0.4465), std=(0.2023, 0.1994, 0.2010),
     return dataset_train, dataset_test
 
 
-def get_cifar100(mean=(0.5071, 0.4867, 0.4408), std=(0.2675, 0.2565, 0.2761),
-                 crop_ratio=(0.875, 0.875), root="./data", download=False):
-    crop_size = (np.array((32, 32)) * (1 - np.array(crop_ratio))).astype(int)
-
+def get_cifar100(mean=(0.5071, 0.4867, 0.4408), std=(0.2675, 0.2565, 0.2761), padding=(4, 4),
+                 root="./data", download=False):
     transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=tuple(crop_size)),
+        transforms.RandomCrop(32, padding=tuple(padding)),
         transforms.RandomHorizontalFlip(),
         transforms.ToTensor(),
         transforms.Normalize(mean, std),
@@ -65,12 +60,12 @@ def get_corruptions():
     return cifarc.CIFAR10C.corruption_list
 
 
-def get_dataset_c(name, ctype, intensity, mean, std,
-                  root="./data", download=False):
+def get_dataset_c(name, ctype, intensity,
+                  root="./data", download=False, **kwargs):
     if name in ["cifar", "cifar10", "cifar-10"]:
-        dataset_c = get_cifar10c(ctype, intensity, mean, std, root=root, download=download)
+        dataset_c = get_cifar10c(ctype, intensity, root=root, download=download, **kwargs)
     elif name in ["cifar100", "cifar-100"]:
-        dataset_c = get_cifar100c(ctype, intensity, mean, std, root=root, download=download)
+        dataset_c = get_cifar100c(ctype, intensity, root=root, download=download, **kwargs)
     else:
         raise NotImplementedError
     return dataset_c
