bea3dde54d7c6e60b18d9233464a83e86dced998
XXXNELL <xxxxxnell@gmail.com>
2021-02-19 16:02:52 +0900

    Add tanh smoothing block

--- models/smoothing_block.py
+++ models/smoothing_block.py
@@ -5,7 +5,7 @@ import models.layers as layers
 
 class SigmoidBlurBlock(nn.Module):
 
-    def __init__(self, in_filters, temp=5e-1, sfilter=(1, 1), pad_mode="constant", **kwargs):
+    def __init__(self, in_filters, temp=1e1, sfilter=(1, 1), pad_mode="constant", **kwargs):
         super(SigmoidBlurBlock, self).__init__()
 
         self.temp = temp
@@ -24,7 +24,7 @@ class SigmoidBlurBlock(nn.Module):
 
 class PreSigmoidBlurBlock(nn.Module):
 
-    def __init__(self, in_filters, temp=2e0, sfilter=(1, 1), pad_mode="constant", **kwargs):
+    def __init__(self, in_filters, temp=1e1, sfilter=(1, 1), pad_mode="constant", **kwargs):
         super(PreSigmoidBlurBlock, self).__init__()
 
         self.temp = temp
@@ -47,7 +47,7 @@ class PreSigmoidBlurBlock(nn.Module):
 
 class BoundedSigmoidBlurBlock(nn.Module):
 
-    def __init__(self, in_filters, temp=2e0, sfilter=(1, 1), pad_mode="constant", **kwargs):
+    def __init__(self, in_filters, temp=1e1, sfilter=(1, 1), pad_mode="constant", **kwargs):
         super(BoundedSigmoidBlurBlock, self).__init__()
 
         self.temp = temp
@@ -66,7 +66,7 @@ class BoundedSigmoidBlurBlock(nn.Module):
 
 class SoftmaxBlurBlock(nn.Module):
 
-    def __init__(self, in_filters, temp=1e0, sfilter=(1, 1), pad_mode="constant", **kwargs):
+    def __init__(self, in_filters, temp=1e1, sfilter=(1, 1), pad_mode="constant", **kwargs):
         super(SoftmaxBlurBlock, self).__init__()
 
         self.temp = temp
@@ -103,7 +103,7 @@ class ReLuBlurBlock(nn.Module):
 
 class ScalingBlock(nn.Module):
 
-    def __init__(self, temp=4*2e0, **kwargs):
+    def __init__(self, temp=1e1, **kwargs):
         super(ScalingBlock, self).__init__()
 
         self.temp = temp
@@ -132,16 +132,35 @@ class ReLuBlock(nn.Module):
         return "thr=%.3e" % self.thr
 
 
+class TanhBlurBlock(nn.Module):
+
+    def __init__(self, in_filters, temp=1e1, sfilter=(1, 1), pad_mode="constant", **kwargs):
+        super(TanhBlurBlock, self).__init__()
+
+        self.temp = temp
+        self.tanh = nn.Tanh()
+        self.blur = layers.blur(in_filters, sfilter=sfilter, pad_mode=pad_mode)
+
+    def forward(self, x):
+        x = self.temp * self.tanh(x / self.temp)
+        x = self.blur(x)
+
+        return x
+
+    def extra_repr(self):
+        return "temp=%.3e" % self.temp
+
+
 class TanhBlock(nn.Module):
 
-    def __init__(self, temp=1e0, **kwargs):
+    def __init__(self, temp=1e1, **kwargs):
         super(TanhBlock, self).__init__()
 
         self.temp = temp
         self.tanh = nn.Tanh()
 
     def forward(self, x):
-        x = self.tanh(x / self.temp)
+        x = self.temp * self.tanh(x / self.temp)
 
         return x
 
