83883a310b33d1c961aa1c9c655179c7bc5209fb
XXXNELL <xxxxxnell@gmail.com>
2021-07-03 19:53:31 +0900

    Add skip to Mixer

--- models/mixer.py
+++ models/mixer.py
@@ -9,13 +9,43 @@ from einops.layers.torch import Reduce
 
 from models.embeddings import PatchEmbedding
 from models.attentions import FeedForward
+from models.layers import StochasticDepth
+
+
+class MixerBlock(nn.Module):
+
+    def __init__(self, hidden_dim, spatial_dim, channel_dim, num_patches,
+                 dropout=0.0, sd=0.0):
+        super().__init__()
+        f1, f2 = partial(nn.Conv1d, kernel_size=1), nn.Linear
+
+        self.norm1 = nn.LayerNorm(hidden_dim)
+        self.ff1 = FeedForward(num_patches, spatial_dim, f=f1, dropout=dropout)
+        self.sd1 = StochasticDepth(sd)
+
+        self.norm2 = nn.LayerNorm(hidden_dim)
+        self.ff2 = FeedForward(hidden_dim, channel_dim, f=f2, dropout=dropout)
+        self.sd2 = StochasticDepth(sd)
+
+    def forward(self, x):
+        skip = x
+        x = self.norm1(x)
+        x = self.ff1(x)
+        x = self.sd1(x, skip)
+
+        skip = x
+        x = self.norm2(x)
+        x = self.ff2(x)
+        x = self.sd2(x, skip)
+
+        return x
 
 
 class Mixer(nn.Module):
 
     def __init__(self, *,
                  image_size, patch_size, num_classes, hidden_dim, spatial_dim, channel_dim, depth,
-                 channel=3, dropout=0.0,
+                 channel=3, dropout=0.0, sd=0.0,
                  name="mixer"):
         super().__init__()
         self.name = name
@@ -26,12 +56,11 @@ class Mixer(nn.Module):
         self.embedding = PatchEmbedding(image_size, patch_size, hidden_dim, channel=channel)
 
         self.mlps = []
-        f1, f2 = partial(nn.Conv1d, kernel_size=1), nn.Linear
-        for _ in range(depth):
-            self.mlps.append(nn.LayerNorm(hidden_dim))
-            self.mlps.append(FeedForward(num_patches, spatial_dim, f=f1, dropout=dropout))
-            self.mlps.append(nn.LayerNorm(hidden_dim))
-            self.mlps.append(FeedForward(hidden_dim, channel_dim, f=f2, dropout=dropout))
+        for i in range(depth):
+            self.mlps.append(
+                MixerBlock(hidden_dim, spatial_dim, channel_dim, num_patches,
+                           dropout=dropout, sd=(sd * i / (depth - 1)))
+            )
         self.mlps = nn.Sequential(*self.mlps)
 
         self.classifier = nn.Sequential(
@@ -51,12 +80,13 @@ class Mixer(nn.Module):
 def tiny(num_classes=1000, name="mixer_ti",
          image_size=224, patch_size=16,
          depth=8, hidden_dim=256, spatial_dim=128, channel_dim=1024,
-         channels=3, dropout=0.0):
+         channels=3, dropout=0.0, sd=0.0,
+         **block_kwargs):
     return Mixer(
         image_size=image_size, patch_size=patch_size, channel=channels,
         num_classes=num_classes, depth=depth,
         hidden_dim=hidden_dim, spatial_dim=spatial_dim, channel_dim=channel_dim,
-        dropout=dropout,
+        dropout=dropout, sd=sd,
         name=name
     )
 
@@ -64,12 +94,13 @@ def tiny(num_classes=1000, name="mixer_ti",
 def small(num_classes=1000, name="mixer_s",
           image_size=224, patch_size=16,
           depth=8, hidden_dim=512, spatial_dim=256, channel_dim=2048,
-          channels=3, dropout=0.0):
+          channels=3, dropout=0.0, sd=0.0,
+         **block_kwargs):
     return Mixer(
         image_size=image_size, patch_size=patch_size, channel=channels,
         num_classes=num_classes, depth=depth,
         hidden_dim=hidden_dim, spatial_dim=spatial_dim, channel_dim=channel_dim,
-        dropout=dropout,
+        dropout=dropout, sd=sd,
         name=name
     )
 
@@ -77,12 +108,13 @@ def small(num_classes=1000, name="mixer_s",
 def base(num_classes=1000, name="mixer_b",
          image_size=224, patch_size=16,
          depth=12, hidden_dim=768, spatial_dim=384, channel_dim=3072,
-         channels=3, dropout=0.0):
+         channels=3, dropout=0.0, sd=0.0,
+         **block_kwargs):
     return Mixer(
         image_size=image_size, patch_size=patch_size, channel=channels,
         num_classes=num_classes, depth=depth,
         hidden_dim=hidden_dim, spatial_dim=spatial_dim, channel_dim=channel_dim,
-        dropout=dropout,
+        dropout=dropout, sd=sd,
         name=name
     )
 
@@ -90,12 +122,13 @@ def base(num_classes=1000, name="mixer_b",
 def large(num_classes=1000, name="mixer_l",
           image_size=224, patch_size=16,
           depth=24, hidden_dim=1024, spatial_dim=512, channel_dim=4096,
-          channels=3, dropout=0.0):
+          channels=3, dropout=0.0, sd=0.0,
+         **block_kwargs):
     return Mixer(
         image_size=image_size, patch_size=patch_size, channel=channels,
         num_classes=num_classes, depth=depth,
         hidden_dim=hidden_dim, spatial_dim=spatial_dim, channel_dim=channel_dim,
-        dropout=dropout,
+        dropout=dropout, sd=sd,
         name=name
     )
 
@@ -103,11 +136,12 @@ def large(num_classes=1000, name="mixer_l",
 def huge(num_classes=1000, name="mixer_h",
          image_size=224, patch_size=16,
          depth=32, hidden_dim=1280, spatial_dim=640, channel_dim=5120,
-         channels=3, dropout=0.0):
+         channels=3, dropout=0.0, sd=0.0,
+         **block_kwargs):
     return Mixer(
         image_size=image_size, patch_size=patch_size, channel=channels,
         num_classes=num_classes, depth=depth,
         hidden_dim=hidden_dim, spatial_dim=spatial_dim, channel_dim=channel_dim,
-        dropout=dropout,
+        dropout=dropout, sd=sd,
         name=name
     )
