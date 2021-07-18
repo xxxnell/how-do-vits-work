f4bd5ee08ae4bfc8c117a7ea8340ed068db58e3e
XXXNELL <xxxxxnell@gmail.com>
2021-07-18 14:54:20 +0900

    Remove BN1D from attention module

--- models/attentions.py
+++ models/attentions.py
@@ -7,7 +7,7 @@ import torch
 from torch import nn, einsum
 
 from einops import rearrange
-from models.layers import bn1d, DropPath
+from models.layers import DropPath
 
 
 class FeedForward(nn.Module):
